b7f160e171bb6cc9e9ae87791f73ccda82c78e16
xoxoinny0 <xoxoinny0@gmail.com>
2024-08-21 22:16:37 +0900

    section2

---
+++ section02/src/pages/_app.tsx
@@ -0,0 +1,36 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push("/test");
+  };
+
+  useEffect(() => {
+    // test 페이지 명시적으로 프리패칭 설정
+    router.prefetch("/test");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
+}
