3f3828bab3e9457d8afa7c91c7f60ee99fd86bed
xoxoinny0 <xoxoinny0@gmail.com>
2024-08-25 17:00:03 +0900

    section02 - SSG 정적 경로 적용

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,16 +1,13 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode, useEffect } from "react";
-import books from "@/mock/books.json";
+import { ReactNode } from "react";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export const getServerSideProps = async () => {
-  // const allBooks = await fetchBooks();
-  // const recoBooks = await fetchRandomBooks();
-
+export const getStaticProps = async () => {
+  console.log("인덱스 페이지");
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -23,8 +20,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log("allBooks", allBooks);
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
