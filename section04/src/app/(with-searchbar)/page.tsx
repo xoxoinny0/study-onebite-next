fc7ffe2343a93e86a8247afb88f8c6b503c36286
xoxoinny0 <xoxoinny0@gmail.com>
2024-09-01 01:35:23 +0900

    4.2) 데이터 캐시

--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -1,11 +1,11 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
 import { BookData } from "@/types";
 
 async function AllBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "no-store" }
   );
   if (!response.ok) {
     1;
@@ -23,7 +23,8 @@ async function AllBooks() {
 
 async function RecoBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
@@ -44,11 +45,11 @@ export default async function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <AllBooks />
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <RecoBooks />
+        <AllBooks />
       </section>
     </div>
   );
