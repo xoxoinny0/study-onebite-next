63008cc6de65626ef553f8208dad783fff88011b
xoxoinny0 <xoxoinny0@gmail.com>
2024-09-02 00:46:24 +0900

    5.5) 클라이언트 라우터 캐시

--- section05/src/app/(with-searchbar)/layout.tsx
+++ section05/src/app/(with-searchbar)/layout.tsx
@@ -4,6 +4,7 @@ import Searchbar from "../../components/searchbar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
+      {/* <div>{new Date().toLocaleString()}</div> */}
       {/* 사전 렌더링에서는 배제, 클라이언트 측에서만 렌더링 되도록 설정 */}
       <Suspense fallback={<div>Loading...</div>}>
         <Searchbar />
