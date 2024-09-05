8fd6790dbeac03dc6c7c0599c8d79e1cc9801957
xoxoinny0 <xoxoinny0@gmail.com>
2024-09-06 01:05:21 +0900

    6.4) 스켈레톤 UI 적용하기

--- section06/src/app/(with-searchbar)/page.tsx
+++ section06/src/app/(with-searchbar)/page.tsx
@@ -3,6 +3,8 @@ import style from "./page.module.css";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
 import { Suspense } from "react";
+import BookItemSkeleton from "@/components/skeleton/book-item-skeleton";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 
 // 특정 페이지의 유형을 강제로 Static,Dynamic 페이지로 설정
 // 1. auto : 기본값, 아무것도 강제하지 않음
@@ -58,13 +60,13 @@ export default async function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <Suspense fallback={<div>도서 불러오는 중..</div>}>
+        <Suspense fallback={<BookListSkeleton count={3} />}>
           <RecoBooks />
         </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <Suspense fallback={<div>도서 불러오는 중..</div>}>
+        <Suspense fallback={<BookListSkeleton count={10} />}>
           <AllBooks />
         </Suspense>
       </section>
