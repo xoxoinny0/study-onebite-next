577c0214686ea1572d2b561e8d76b63bcb16dc66
xoxoinny0 <xoxoinny0@gmail.com>
2024-09-15 00:04:14 +0900

    9.1) 이미지 최적화

---
+++ section09/src/components/review-item.tsx
@@ -0,0 +1,22 @@
+import { ReviewData } from "@/types";
+import style from "./review-item.module.css";
+import ReviewItemDeleteBtn from "./review-item-delete-btn";
+
+export default function ReviewItem({
+  id,
+  content,
+  author,
+  createdAt,
+  bookId,
+}: ReviewData) {
+  return (
+    <div className={style.container}>
+      <div className={style.author}>{author}</div>
+      <div className={style.content}>{content}</div>
+      <div className={style.bottom_container}>
+        <div className={style.date}>{new Date(createdAt).toLocaleString()}</div>
+        <ReviewItemDeleteBtn reviewId={id} bookId={bookId} />
+      </div>
+    </div>
+  );
+}
